7beac0ea5eb9fbf9df2552f28761af72367f2e56
Will <wmacaulay@gmail.com>
2011-08-11 14:42:52 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Saucesite::Application.config.secret_token = '3a3f3417bdb10792df5289b0f46b94e50d1ffc0e1a3dc1aa75eb1315bd92de6e66c554b9577be551407336aba19c17bcad8591861a27f490f652fc80e9004670'
